efc49ef90b226446a270ef5866758bb862614dcc
supreme2302 <31735029+supreme2302@users.noreply.github.com>
2017-12-21 02:17:05 +0300

    Add files via upload

---
+++ Menu.h
@@ -0,0 +1,48 @@
+#pragma once
+void menu(RenderWindow & window) {
+	Texture menuTexture1, menuTexture2, menuTexture3, aboutTexture, menuBackground;
+	menuTexture1.loadFromFile("images/111.png");
+	menuTexture2.loadFromFile("images/222.png");
+	menuTexture3.loadFromFile("images/333.png");
+	aboutTexture.loadFromFile("images/about.jpg");
+	menuBackground.loadFromFile("images/Motherland.jpg");
+	Sprite menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), about(aboutTexture), menuBg(menuBackground);
+	bool isMenu = 1;
+	int menuNum = 0;
+	about.setScale(1.56, 1.17);
+	menu1.setPosition(500, 30);
+	menu2.setPosition(500, 90);
+	menu3.setPosition(500, 150);
+	menuBg.setPosition(0, 0);
+	
+
+	//////////////////////////////МЕНЮ///////////////////
+	while (isMenu)
+	{
+		menu1.setColor(Color::White);
+		menu2.setColor(Color::White);
+		menu3.setColor(Color::White);
+		menuNum = 0;
+		//window.clear(Color(220, 51, 35));
+
+		if (IntRect(500, 30, 200, 30).contains(Mouse::getPosition(window))) { menu1.setColor(Color::Red); menuNum = 1; }
+		if (IntRect(500, 90, 200, 30).contains(Mouse::getPosition(window))) { menu2.setColor(Color::Red); menuNum = 2; }
+		if (IntRect(500, 150, 200, 30).contains(Mouse::getPosition(window))) { menu3.setColor(Color::Red); menuNum = 3; }
+
+		if (Mouse::isButtonPressed(Mouse::Left))
+		{
+			if (menuNum == 1) isMenu = false;//если нажали первую кнопку, то выходим из меню 
+			if (menuNum == 2) { window.draw(about); window.display(); while (!Keyboard::isKeyPressed(Keyboard::Escape)); }
+			if (menuNum == 3) { window.close(); isMenu = false; }
+
+		}
+
+		window.draw(menuBg);
+		window.draw(menu1);
+		window.draw(menu2);
+		window.draw(menu3);
+
+		window.display();
+	}
+	////////////////////////////////////////////////////
+}
--- interObj.h
+++ interObj.h
@@ -1,77 +1,50 @@
+#pragma once
+#include "level.h"
 class interObj
 {
 protected:
-	float dx, dy;
-	FloatRect rect;
+	String name;
+	float dx, dy, speed;
+	float x, y;
+	int w, h;
 	bool onGround;
 	String path;
 	Image img;
 	Texture text;
 	Sprite sprite;
 	float currentFrame;
+	std::vector<Object> obj;//вектор объектов карты
 public:
-	interObj(String p, float sx, float sy, FloatRect r, bool onG, float cuF)
+	interObj(String p, String Name, int W, int H, float X, float Y)
 	{
 		path = p;
 		img.loadFromFile(path);
 		text.loadFromImage(img);
 		sprite.setTexture(text);
-		dx = sx;
-		dy = sy;
-		rect = r;
-		onGround = onG;
-		currentFrame = cuF;
+		sprite.setTextureRect(IntRect(40 * int(currentFrame), 0, 40, 60));
+		dx = 0;
+		dy = 0;
+		x = X;
+		y = Y;
+		w = W;
+		h = H;
+		onGround = false;
+		currentFrame = 0;
+		speed = 0;
+		name = Name;
 	};
-
-
-	virtual void Update(float time) = 0;
-
-
-
-	void Collision(int dir)
+	
+	virtual FloatRect getRect()
 	{
-		for (int i = rect.top / 32; i<(rect.top + rect.height) / 32; i++)
-			for (int j = rect.left / 32; j<(rect.left + rect.width) / 32; j++)
-			{
-				if (TileMap[i][j] == 'B' || TileMap[i][j] == 'O' || TileMap[i][j] == 'W')
-				{
-					if ((dx>0) && (dir == 0)) rect.left = j * 32 - rect.width;
-					if ((dx<0) && (dir == 0)) rect.left = j * 32 + 32;
-					if ((dy>0) && (dir == 1)) { rect.top = i * 32 - rect.height;  dy = 0;   onGround = true; }
-					if ((dy<0) && (dir == 1)) { rect.top = i * 32 + 32;   dy = 0; }
-				}
-
-				if (TileMap[i][j] == '0')
-				{
-					TileMap[i][j] = ' ';
-				}
-
-			}
-
+		return FloatRect(x, y, w, h);
 	}
-	void SetDx(float _dx)
+
+
+	virtual void Update(float time, bool) = 0;
+
+	String GetName()
 	{
-		dx = _dx;
-	}
-	void SetDy(float _dy)
-	{
-		dy = _dy;
-	}
-	void SetOnGround(bool _onGround)
-	{
-		onGround = _onGround;
-	}
-	bool GetOnGround()
-	{
-		return onGround;
-	}
-	float GetX()
-	{
-		return rect.left;
-	}
-	float GetY()
-	{
-		return rect.top;
+		return name;
 	}
 	Sprite GetS()
 	{
@@ -81,85 +54,156 @@ public:
 
 class Player : public interObj
 {
+	enum { left, right, up, down, jump, stay } state;
+	float count_time;
+	bool dang; // флаг для выбора анимации;
 public:
-	Player(String p, float sx, float sy, FloatRect r, bool onG, float cuF) : interObj(p, sx, sy, r, onG, cuF){}
-	void Update(float time);
+	Player(String P, String Name, Level &lev, int W, int H, float X, float Y) : interObj(P, Name, W, H, X, Y)
+	{
+		obj = lev.GetAllObjects();
+		count_time = 0;
+		dang = false;
+	}
+	void Update(float time, bool danger);
+	void Collision(float Dx, float Dy);
+	void Control();
+	bool Interrupt(interObj* obj, float time);
+	void Animation(float time, bool danger);
 };
-void Player::Update(float time)
+void Player::Animation(float time, bool danger)
 {
-	rect.left += dx * time;  //rect.left - РєРѕРѕСЂРґРёРЅР°С‚Р° С…
-	Collision(0);   //РѕР±СЂР°Р±РѕС‚РєР° СЃС‚РѕР»РєРЅРѕРІРµРЅРёР№ РїРѕ С…
+	if (danger == false) {
+		currentFrame += 0.008*time;
+		if (currentFrame > 4) currentFrame -= 4;
+
+		if (dx>0) sprite.setTextureRect(IntRect(40 * int(currentFrame), 0, 40, 60));
+		if (dx<0) sprite.setTextureRect(IntRect(40 * int(currentFrame) + 40, 0, -40, 60));
+	}
+	else {
+		currentFrame += 0.008*time;
+		if (currentFrame > 8) currentFrame -= 4;
+
+		if (dx>0) sprite.setTextureRect(IntRect(40 * int(currentFrame), 0, 40, 60));
+		if (dx<0) sprite.setTextureRect(IntRect(40 * int(currentFrame) + 40, 0, -40, 60));
+	}
+	
+}
+void Player::Update(float time, bool danger)
+{
+	Control();
+	switch (state)
+	{
+	case right:dx = speed; break;
+	case left:dx = -speed; break;
+	case up: break;
+	case down: dx = 0; break;
+	case stay: break;
+	}
+	x += dx * time;  //rect.left - координата х
+	Collision(dx, 0);   //обработка столкновений по х
 
 	if (!onGround) dy = dy + 0.0005*time;
-	rect.top += dy*time;   //rect.top  - РєРѕРѕСЂРґРёРЅР°С‚Р° y
+	y += dy*time;   //rect.top  - координата y
 	onGround = false;
-	Collision(1); //РѕР±СЂР°Р±РѕС‚РєР° СЃС‚РѕР»РєРЅРѕРІРµРЅРёР№ РїРѕ Сѓ
+	Collision(0, dy); //обработка столкновений по у
 
-
-	currentFrame += 0.008*time;
+	Animation(time, danger);
+	/*currentFrame += 0.008*time;
 	if (currentFrame > 4) currentFrame -= 4;
 
 	if (dx>0) sprite.setTextureRect(IntRect(40 * int(currentFrame), 0, 40, 60));
-	if (dx<0) sprite.setTextureRect(IntRect(40 * int(currentFrame) + 40, 0, -40, 60));
+	if (dx<0) sprite.setTextureRect(IntRect(40 * int(currentFrame) + 40, 0, -40, 60));*/
 
 
-	sprite.setPosition(rect.left, rect.top);
+	sprite.setPosition(x, y);
+	GetPlayerCoordianteForView(x, y);
 
-	dx = 0; //РґР»СЏ РѕСЃС‚Р°РЅРѕРІРєРё РїРµСЂСЃРѕРЅР°Р¶Р°
+	state = stay;
+	dx = 0; //для остановки персонажа
+}
+void Player::Collision(float Dx, float Dy)
+{
+	for (int i = 0; i < obj.size(); i++)
+	{//проходимся по объектам 
+		if (getRect().intersects(obj[i].rect))//проверяем пересечение игрока с объектом
+		{
+			if (obj[i].name == "solid")//если встретили препятствие
+			{
+				if (Dy > 0) { y = obj[i].rect.top - h;  dy = 0; onGround = true; }
+				if (Dy < 0) { y = obj[i].rect.top + obj[i].rect.height;   dy = 0; }
+				if (Dx > 0) { x = obj[i].rect.left - w; }
+				if (Dx < 0) { x = obj[i].rect.left + obj[i].rect.width; }
+			}
+
+		}
+	}
+
+}
+bool Player::Interrupt(interObj* object, float time)
+{
+	count_time += time;
+	//std::cout << count_time << "\n";
+	if (getRect().intersects(object->getRect())) {
+		if (object->GetName() == "enemy"  && !dang) {
+			std::cout << "ok" << std::endl;
+			count_time = 0;
+			dang = true;
+		}
+	}
+	if (count_time > 15000)
+		dang = false;
+
+	return dang;
+}
+void Player::Control() {
+	 
+		if (Keyboard::isKeyPressed(Keyboard::Left)) {
+			state = left; 
+			speed = 0.1;
+			//dx = -0.1;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Right)) {
+			state = right; 
+			speed = 0.1;
+			//dx = 0.1;
+		}
+	
+
+		if ((Keyboard::isKeyPressed(Keyboard::Up)) && (onGround)) {
+			state = jump; 
+			dy = -0.35; onGround = false;
+		}
+
+		/*if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			state = down;
+		}*/
+	
 }
 
 class Enemy : public interObj
 {
 	int moveTimer;
 public:
-	Enemy(String p, float sx, float sy, FloatRect r, bool onG, float cuF) : interObj(p, sx, sy, r, onG, cuF) {
+	Enemy(String P, String Name, Level &lev, int W, int H, float X, float Y) : interObj(P, Name, W, H, X, Y) {
 		dx = 0.1; moveTimer = 0;
 		dy = 0;
+		obj = lev.GetAllObjects();
+		sprite.setTextureRect(IntRect(40 * 4, 0, 40, 60));
 	}
-	void Update(float time);
-	void Collision(int dir, float time)
-	{
-		for (int i = rect.top / 32; i<(rect.top + rect.height) / 32; i++)
-			for (int j = rect.left / 32; j<(rect.left + rect.width) / 32; j++)
-			{
-				if (TileMap[i][j] == 'B' || TileMap[i][j] == 'O' || TileMap[i][j] == 'W')
-				{
-					if ((dx > 0) && (dir == 0)) { rect.left = j * 32 - rect.width; }
-					if ((dx < 0) && (dir == 0)) { rect.left = j * 32 + 32;  }
-					if ((dy>0) && (dir == 1)) { rect.top = i * 32 - rect.height;  dy = 0;   onGround = true; }
-					if ((dy<0) && (dir == 1)) { rect.top = i * 32 + 32;   dy = 0; }
-				}
-				else if (TileMap[i][j] == 'R' && dy == 0 && dx > 0)
-				{
-					dy = -0.35;
-				}
-				else if (TileMap[i][j] == 'L' && dy == 0 && dx < 0)
-				{
-					dy = -0.35;
-				}
-
-				if (TileMap[i][j] == '0')
-				{
-					TileMap[i][j] = ' ';
-				}
-
-			}
-
-	}
+	void Update(float time, bool);
+	void Collision(float Dx, float Dy);
+	
 
 };
-void Enemy::Update(float time)
+void Enemy::Update(float time, bool danger)
 {
-	
-	moveTimer += time;
-	std::printf("%d",moveTimer); 
-	if (moveTimer>8500) { dx *= -1; moveTimer = 0; }
-	rect.left += dx * time;  //rect.left - РєРѕРѕСЂРґРёРЅР°С‚Р° С…
-	Collision(0 , time);   //РѕР±СЂР°Р±РѕС‚РєР° СЃС‚РѕР»РєРЅРѕРІРµРЅРёР№ РїРѕ С…
+	x += dx * time;  //rect.left - координата х
+	Collision(dx, 0);   //обработка столкновений по х
+
 	if (!onGround) dy = dy + 0.0005*time;
-	rect.top += dy*time;   //rect.top  - РєРѕРѕСЂРґРёРЅР°С‚Р° y
+	y += dy*time;   //rect.top  - координата y
 	onGround = false;
-	Collision(1, time); //РѕР±СЂР°Р±РѕС‚РєР° СЃС‚РѕР»РєРЅРѕРІРµРЅРёР№ РїРѕ Сѓ
+	Collision(0, dy); //обработка столкновений по у
 
 
 	currentFrame += 0.008*time;
@@ -169,7 +213,21 @@ void Enemy::Update(float time)
 	if (dx<0) sprite.setTextureRect(IntRect(40 * int(currentFrame) + 40, 0, -40, 60));
 
 
-	sprite.setPosition(rect.left, rect.top);
+	sprite.setPosition(x, y);
+	//GetPlayerCoordianteForView(x, y);
+}
+void Enemy::Collision(float Dx, float Dy)
+{
+	for (int i = 0; i<obj.size(); i++)//проходимся по объектам
+		if (getRect().intersects(obj[i].rect))//проверяем пересечение игрока с объектом
+		{
+			if (obj[i].name == "solid")//если встретили препятствие
+			{
+				if (Dy>0) { y = obj[i].rect.top - h;  dy = 0; onGround = true; }
+				if (Dy<0) { y = obj[i].rect.top + obj[i].rect.height;   dy = 0; }
+				if (Dx > 0) { x = obj[i].rect.left - w; dx = -dx; }
+				if (Dx < 0) { x = obj[i].rect.left + obj[i].rect.width; dx = -dx; }
+			}
 
-	//dx = 0; //РґР»СЏ РѕСЃС‚Р°РЅРѕРІРєРё РїРµСЂСЃРѕРЅР°Р¶Р°
+		}
 }
